ece115007ad1a1d71a1a8174a7777cfdb8648a2e
CamargoBruno <bcncamargo@icloud.com>
2026-06-11 21:27:16 -0300

    Alteracao de Teste

--- Teste/main.py
+++ Teste/main.py
@@ -1,2 +1,12 @@
 test the commited
 criei esse texto após o commit
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f"Olá, {nome}!")
+print(f"Você tem {idade} anos.")
+
+if int(idade) >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
